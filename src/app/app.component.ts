8128f0a6f42e72e7eb712e74ba7615b207dc1a80
GregoireLgt <gregoire.laget@reseau.eseo.fr>
2021-01-26 15:10:13 +0100

    Add possibility to create a new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import {CdkDragDrop, transferArrayItem} from '@angular/cdk/drag-drop';
+import { MatDialog} from '@angular/material/dialog';
+import {TaskDialogComponent, TaskDialogResult} from './task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +15,34 @@ export class AppComponent {
     { title: 'Task 2', description: 'Learn React'},
     { title: 'Task 3', description: 'Get ready for CDC'}
   ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  constructor(private dialog: MatDialog) {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return; // task is in the same container
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
+  edit(list: string, task: Task): void {
+  }
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {}
+      }
+      });
+    dialogRef.afterClosed()
+      .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { TaskComponent } from './task/task.component';
 import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button'
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import  { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
